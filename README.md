6f84ebe05cb2dff4b5c7b46855221c1ffd7895e0
eramirezcarlos <tequendama.developer@gmail.com>
2023-09-30 12:23:24 -0400

    small adjustment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+## getStaticProps and getStaticPaths Improved by calling get data
+
+we are hidrating all the ids from the file, impractical but just for the sake of checking this exercise
+
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
--- pages/products/[pid].js
+++ pages/products/[pid].js
@@ -21,14 +21,25 @@ function ProductDetailPage( props ){
     );
 
 }
+async function getData(){
+
+    const filePath = path.join(process.cwd(),'data','dummy-backend.json');  
+    const jsonData = await  fs.readFile( filePath );
+    const data = JSON.parse( jsonData ); 
+
+    return data;
+
+}
+
 export async function getStaticProps( context ){
 
     const { params } = context;
 
-    const productId = params.pid;
-    const filePath = path.join(process.cwd(),'data','dummy-backend.json');  
-    const jsonData = await  fs.readFile( filePath );
-    const data = JSON.parse( jsonData ); 
+    let data;
+
+    const productId = params.pid; 
+
+    data = await getData();
 
     const product = data.products.find( product => product.id === productId );
 
@@ -42,16 +53,14 @@ export async function getStaticProps( context ){
 
 export async function getStaticPaths() {
 
-    const paths = 
-        [
-            { params: { pid: 'p1' } },
-            { params: { pid: 'p2' } },
-            { params: { pid: 'p3' } },
-        ];
+    const data = await getData();
+    const ids = data.products.map(product => product.id);
+    const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
+
 
     return {
-        paths,
-        fallback: false,
+        paths: pathsWithParams,
+        fallback: 'blocking',
     };
 }
 
