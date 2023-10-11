c9d8814da312601e8b9c3897fec4d18b6f966c9e
eramirezcarlos <tequendama.developer@gmail.com>
2023-10-10 20:22:43 -0400

    static, dynamic or static and Client sdie with  pre-rendering a couple of recors

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,38 +1,65 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+function LastSalesPage( props ) {
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
+  //we can customize the function to format the data with function fetcher of swr here we use useEffect hook
+  const { data, error } = useSWR(
+    "https://training-nextjs-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    fetch(
-      "https://training-nextjs-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading( false );
-      });
-  }, []);
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if( isLoading){
-    return <p>Loading ...</p>
-  }
-  if( !sales){
-    return <p>No data yet</p>
+  if (error) {
+    return <p>Failed to load </p>;
   }
 
-  return( 
-  <ul>
-    {sales.map( sale => <li key={sale.id}> {sale.username} - %{sale.volume}</li>)}
-  </ul>)
-  ;
+  if (!data && !sales) {
+    return <p>Loading ...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - %{sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+
+  const response = await fetch(
+    "https://training-nextjs-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales } };
 }
 export default LastSalesPage;
